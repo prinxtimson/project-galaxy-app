e4e01d6e180da089a766b483a3be13ab1176df11
mac <timson.babajide@gmail.com>
2022-08-15 09:53:00 +0100

    update terms page

--- src/pages/TermsConditions.jsx
+++ src/pages/TermsConditions.jsx
@@ -7,6 +7,21 @@ const TermsConditions = () => {
         <div className="mb-5">
           <h2>Terms and Conditions</h2>
         </div>
+        <div className="mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+          venenatis, neque id iaculis dignissim, erat felis lacinia ligula, sed
+          posuere mi nunc nec metus. Ut at diam vitae lectus elementum
+          consequat. Etiam non ultricies lacus, ac pellentesque sem. Mauris
+          fermentum, tortor sit amet facilisis elementum, lectus tellus laoreet
+          enim, sit amet scelerisque erat quam sed turpis. Donec ultricies
+          mollis ipsum ac ultricies. Integer non luctus metus, in feugiat ante.
+          Morbi vehicula lectus sit amet ante accumsan facilisis. Sed vulputate
+          elementum imperdiet. Ut imperdiet, lorem in scelerisque sodales, enim
+          leo sagittis tortor, ut bibendum dui ligula faucibus turpis. Donec
+          ullamcorper rutrum laoreet. Fusce at enim vehicula, tempus sem vitae,
+          efficitur eros. Suspendisse sed mattis eros. Sed malesuada eu est in
+          blandit. Vivamus nec augue nunc.
+        </div>
         <div className="">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
           venenatis, neque id iaculis dignissim, erat felis lacinia ligula, sed
